c05b31a725d64767186e5fb5a9a43689ff1ea4d6
Avinash Changrani <74554800+nabobery@users.noreply.github.com>
2022-03-26 14:40:13 +0530

    Added Problem 704 of LeetCode

---
+++ LeetCode/701 - 800/704.cpp
@@ -0,0 +1,17 @@
+// 704. Binary Search
+// O(nlogn) basic binary search
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) high = mid-1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+};
+
